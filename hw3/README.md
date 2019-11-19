20244cea7326e0056b9862f5312c6004c6a1f305
yilin <yilin96cc@gmail.com>
2019-11-20 00:46:23 +0800

    initial commit

---
+++ hw3/README.md
@@ -0,0 +1,64 @@
+# When to Buy or Sell?
+## myOptimAction.py
+the strategy to try to maximize return rate (rr) <br>
+I use DP! <br>
+<br>
+
+### each matrix
+- priceMat: 
+    - An m×n matrix which holds n stocks' price over m days. 
+    - That is, each of the n columns is the price vector of m days for a specific stock.
+- transFeeRate: Rate for transaction fee, which is usually 1/100.
+- actionMat: 
+    - An k×4 action matrix which holds k transaction records. 
+    - In particular, each row of [d,a,b,z] represents a transaction record, as explained next:
+        - d: The index of day, starting from 0 with monotonically increasing values.
+        - a: The index of "from" stock, with "-1" being "cash" and all the other integers being stock index (within [0,n−1]).
+        - b: The index of "to" stock, with "-1 being "cash" and all the other integers being stock index (within [0,n−1]).
+        - z: The equivalent cash for such transaction. This value must be positive.
+    - For instance:
+        - [5, -1, 7, 100]: 
+            - At day 5, use cash of 100 dollars to buy stock 7.
+        - [3, 8, -1, 50]: 
+            - At day 3, sell stock 8 to have cash of 50(1−ρ), where ρ is the rate for transaction fee.
+        - [12, 6, 9, 80]: 
+            - At day 12, sell stock 6 and buy stock 9. 
+            - Such a transaction between stocks incurs two-time transaction fees. 
+            - That is, the cash equivalent of stock 6 you sell is 80(1−ρ), and that of stock 9 you buy is 80(1−ρ)2.
+
+### the DP matrix
+- CMat (dimension: 1*dataLen)
+- SMat (dimension: 4*dataLen, since there are four stocks)
+
+#### the DP initialization
+- CMat[0] = 1000 (original cash is 1000)
+- SMat[0][0] = 1000/priceMat[0][0]
+- SMat[0][1] = 1000/priceMat[0][1]
+- SMat[0][2] = 1000/priceMat[0][2]
+- SMat[0][3] = 1000/priceMat[0][3]
+
+#### to fill other DP entries
+- assume the ith day:
+- CMat[i] = max{yesterday's cash, sell yesterday's stock (there are four
+    options) to get cash}
+- SMat[i][j] = max{yesterday's stock j, use yesterday's cash to buy stock j, sell other
+    stocks (three options) to buy stock j}
+
+### the record matrix (record DP entries)
+- CFrom (dimension: 1*dataLen)
+    - if yesterday's cash (hold cash): -1
+    - if sell yesterday's stock j: j
+- SFrom (dimension: 4*dataLen)
+    - if yesterday's stock j (hold stock): j
+    - if yesterday's cash to buy stock j: -1
+    - if sell yesterday's other stock k to buy stock j: k
+ 
+### to run the main program: <br>
+- `$ python rrEstimateOpen.py priceMat.txt 0.01` <br>
+<br>
+
+## rrEstimateOpen.py
+the main program to estimate the rr
+
+## priceMat.txt
+the price of four stocks
---
+++ hw3/myOptimAction.py
@@ -0,0 +1,141 @@
+import numpy as np
+
+def myOptimAction(priceMat, transFeeRate):
+    # user definition
+    dataLen, stockCount = priceMat.shape # 3236, 4
+    actionMat = []
+
+    # default
+    cash = 1000
+
+    ## DP
+    # stock/cash matrix
+    CMat, SMat = np.zeros(dataLen), np.zeros((dataLen, stockCount))
+    # where the cash/stock comes from
+    CFrom, SFrom = np.zeros(dataLen), np.zeros((dataLen, stockCount))
+
+    # DP matrix initialization
+    CMat[0] = cash
+    SMat[0][0] = cash*(1-transFeeRate)/priceMat[0][0]
+    SMat[0][1] = cash*(1-transFeeRate)/priceMat[0][1]
+    SMat[0][2] = cash*(1-transFeeRate)/priceMat[0][2]
+    SMat[0][3] = cash*(1-transFeeRate)/priceMat[0][3]
+
+    # fill the DP table
+    for i in range(1, dataLen):
+        dayPrices = priceMat[i]
+
+        ## optimal cash
+        holdCash, sellStockCash, which = 0, 0, -1
+        for j in range(stockCount):
+            # two options: {yesterday's cash, sell stock to get cash}
+            hold = CMat[i-1]
+            sell = SMat[i-1][j]*(dayPrices[j])*(1-transFeeRate)
+
+            if hold > holdCash:
+                holdCash = hold
+
+            if sell > sellStockCash:
+                sellStockCash = sell
+                which = j
+
+        maxCash = max(holdCash, sellStockCash)
+        if maxCash == holdCash:
+            CMat[i] = holdCash
+            CFrom[i] = -1 # cash
+        elif maxCash == sellStockCash:
+            CMat[i] = sellStockCash
+            CFrom[i] = which # stock
+
+        ## optimal stock
+        for j in range(stockCount):
+            # three options:
+            # {yesterday's stock, buy stock with cash, sell other stock and buy
+            # the specific stock}
+            holdStock = SMat[i-1][j]
+            buyWithCash = CMat[i-1]*(1-transFeeRate)/dayPrices[j]
+            sellOtherStock, which = 0, -1
+            for k in range(stockCount):
+                if k != j:
+                    val = SMat[i-1][k]*dayPrices[k]*(1-transFeeRate)
+                    val = val*(1-transFeeRate)/dayPrices[j]
+                    if val > sellOtherStock:
+                        sellOtherStock, which = val, k
+
+            maxStock = max(holdStock, buyWithCash, sellOtherStock)
+            if maxStock == holdStock:
+                SMat[i][j] = holdStock
+                SFrom[i][j] = j # stock j
+            elif maxStock == sellOtherStock:
+                SMat[i][j] = sellOtherStock
+                SFrom[i][j] = which # stock k
+            elif maxStock == buyWithCash:
+                SMat[i][j] = buyWithCash
+                SFrom[i][j] = -1 # cash
+
+    ## fill action matrix
+    reversedAction = []
+
+    # the best solution
+    bestVal = CMat[-1]
+    where = CFrom[-1]
+    toType = 'C'
+
+    # trace back
+    i == dataLen-1
+    while(i >= 0):
+        if toType == 'C':
+            if where == -1: # hold cash
+                toType = 'C'
+                bestVal = CMat[i]
+                where = CFrom[i]
+            else: # sell stock to get cash
+                toType = 'S'
+                idx = int(where)
+                equivCash = SMat[i][idx]*priceMat[i+1][idx]
+                bestVal = SMat[i][idx]
+                where = SFrom[i][idx]
+                reversedAction.append([i+1, idx, -1, equivCash])
+                #print([i+1, idx, -1, equivCash])
+
+        else: # toType == 'S'
+            if where == -1: # use cash to buy stock
+                to = -5
+                for j in range(stockCount):
+                    if CMat[i]*(1-transFeeRate)/priceMat[i+1][j] == bestVal:
+                        to = j
+                        break
+                reversedAction.append([i+1, -1, to, CMat[i]])
+                #print([i+1, -1, to, CMat[i]/(1-transFeeRate)])
+                toType = 'C'
+                bestVal = CMat[i]
+                where = CFrom[i]
+            else: # hold stock, or sell other stock to buy stock
+                toType = 'S'
+                idx = int(where)
+                if SMat[i][idx] == bestVal: # hold stock
+                    bestVal = SMat[i][idx]
+                    where = SFrom[i][idx]
+                else: # sell other stock to buy stock
+                    to = -5
+                    for k in range(stockCount):
+                        if idx != k:
+                            tmp = SMat[i][idx]*priceMat[i+1][idx]*(1-transFeeRate)
+                            tmp = tmp*(1-transFeeRate)/priceMat[i+1][k]
+                            if tmp == bestVal:
+                                to = k
+                                break
+
+                    equivCash = SMat[i][idx]*priceMat[i+1][idx]
+                    reversedAction.append([i+1, idx, to, equivCash])
+                    #print([i+1, idx, to, equivCash])
+                    bestVal = SMat[i][idx]
+                    where = SFrom[i][idx]
+
+        i = i-1
+
+    for i in reversed(range(len(reversedAction))):
+        actionMat.append(reversedAction[i])
+        #print(reversedAction[i])
+
+    return actionMat
